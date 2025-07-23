538ce608e4619a78fe95ef2e33f668cf0d31d411
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-07-23 18:27:58 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -10,28 +10,31 @@
 
 enabled_site_setting :add_title_based_on_trust_level_enabled
 
+
+tl0_title = SiteSetting.tl0_title_on_create
+tl0_title = SiteSetting.tl0_title_on_create
+tl1_title = SiteSetting.tl1_title_on_promotion
+tl2_title = SiteSetting.tl2_title_on_promotion
+tl3_title = SiteSetting.tl3_title_on_promotion
+tl4_title = SiteSetting.tl4_title_on_promotion
+
 module ::AddTitleBasedOnTrustLevel
   PLUGIN_NAME = "add-title-based-on-trust-level"
-end
-
-#require_relative "lib/add_title_based_on_trust_level/engine"
-
-after_initialize do
-  tl0_title = SiteSetting.tl0_title_on_create
-  tl0_title = SiteSetting.tl0_title_on_create
-  tl1_title = SiteSetting.tl1_title_on_promotion
-  tl2_title = SiteSetting.tl2_title_on_promotion
-  tl3_title = SiteSetting.tl3_title_on_promotion
-  tl4_title = SiteSetting.tl4_title_on_promotion
-  def update_all_titles
+  def self.update_all_titles
     User.where(trust_level: 0).update_all(title: tl0_title)
     User.where(trust_level: 1).update_all(title: tl1_title)
     User.where(trust_level: 2).update_all(title: tl2_title)
     User.where(trust_level: 3).update_all(title: tl3_title)
     User.where(trust_level: 4).update_all(title: tl4_title)
   end
+end
 
-  update_all_titles
+#require_relative "lib/add_title_based_on_trust_level/engine"
+
+
+
+after_initialize do
+  ::AddTitleBasedOnTrustLevel.update_all_titles
   
   on(:user_created) do |newuserdata|
     newuserid = newuserdata[:user_id]
