060a1d7c2e5b0025b0d2f3c7d46b1ba2d9f16d9f
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-08-02 16:44:56 +0800

    Update update-all-titles.rb

--- app/jobs/scheduled/update-all-titles.rb
+++ app/jobs/scheduled/update-all-titles.rb
@@ -3,6 +3,11 @@ module FindTopicsToReset
     every 12.hours # Run twice a day
 
     def execute(args)
+      tl0_title = SiteSetting.tl0_title_on_create
+      tl1_title = SiteSetting.tl1_title_on_promotion
+      tl2_title = SiteSetting.tl2_title_on_promotion
+      tl3_title = SiteSetting.tl3_title_on_promotion
+      tl4_title = SiteSetting.tl4_title_on_promotion
       User.where(trust_level: 0).update_all(title: tl0_title)
       User.where(trust_level: 1).update_all(title: tl1_title)
       User.where(trust_level: 2).update_all(title: tl2_title)
