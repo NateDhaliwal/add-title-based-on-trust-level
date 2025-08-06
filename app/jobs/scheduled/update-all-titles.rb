7e69188563b856cc9bd0bf8ac1af93ebee5d9724
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-08-06 18:38:32 +0800

    Update update-all-titles.rb

--- app/jobs/scheduled/update-all-titles.rb
+++ app/jobs/scheduled/update-all-titles.rb
@@ -9,10 +9,12 @@ module AddTitleBasedOnTrustLevel
       tl1_title = SiteSetting.tl1_title_on_promotion
       tl2_title = SiteSetting.tl2_title_on_promotion
       tl3_title = SiteSetting.tl3_title_on_promotion
-      User.where(trust_level: 0).update_all(title: tl0_title)
-      User.where(trust_level: 1).update_all(title: tl1_title)
-      User.where(trust_level: 2).update_all(title: tl2_title)
-      User.where(trust_level: 3).update_all(title: tl3_title)
+      tl4_title = SiteSetting.tl4_title_on_promotion
+      User.where(trust_level: 0).update_all(title: tl0_title) if tl0_title != ""
+      User.where(trust_level: 1).update_all(title: tl1_title) if tl1_title != ""
+      User.where(trust_level: 2).update_all(title: tl2_title) if tl2_title != ""
+      User.where(trust_level: 3).update_all(title: tl3_title) if tl3_title != ""
+      User.where(trust_level: 4).update_all(title: tl4_title) if tl4_title != ""
 
       trust_levels = [0, 1, 2, 3, 4]
       if SiteSetting.add_primary_group_title
@@ -27,11 +29,11 @@ module AddTitleBasedOnTrustLevel
           SQL
         end
         # Append the TL text
-        User.where(trust_level: 0).update_all("title = title || ' #{tl0_title}'")
-        User.where(trust_level: 1).update_all("title = title || ' #{tl1_title}'")
-        User.where(trust_level: 2).update_all("title = title || ' #{tl2_title}'")
-        User.where(trust_level: 3).update_all("title = title || ' #{tl3_title}'")
-        User.where(trust_level: 4).update_all("title = title || ' #{tl4_title}'")
+        User.where(trust_level: 0).update_all("title = title || ' #{tl0_title}'") if tl0_title != ""
+        User.where(trust_level: 1).update_all("title = title || ' #{tl1_title}'") if tl1_title != ""
+        User.where(trust_level: 2).update_all("title = title || ' #{tl2_title}'") if tl2_title != ""
+        User.where(trust_level: 3).update_all("title = title || ' #{tl3_title}'") if tl3_title != ""
+        User.where(trust_level: 4).update_all("title = title || ' #{tl4_title}'") if tl4_title != ""
       end
     end
   end
