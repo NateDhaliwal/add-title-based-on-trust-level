d2a491e328cb4c5c6135b3653867fc3d7139e925
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-08-07 08:05:29 +0800

    Update update-all-titles.rb

--- app/jobs/scheduled/update-all-titles.rb
+++ app/jobs/scheduled/update-all-titles.rb
@@ -10,30 +10,70 @@ module AddTitleBasedOnTrustLevel
       tl2_title = SiteSetting.tl2_title_on_promotion
       tl3_title = SiteSetting.tl3_title_on_promotion
       tl4_title = SiteSetting.tl4_title_on_promotion
-      User.where(trust_level: 0).update_all(title: tl0_title) if tl0_title != ""
-      User.where(trust_level: 1).update_all(title: tl1_title) if tl1_title != ""
-      User.where(trust_level: 2).update_all(title: tl2_title) if tl2_title != ""
-      User.where(trust_level: 3).update_all(title: tl3_title) if tl3_title != ""
-      User.where(trust_level: 4).update_all(title: tl4_title) if tl4_title != ""
 
       trust_levels = [0, 1, 2, 3, 4]
       if SiteSetting.add_primary_group_title
-        trust_levels.each do |tl| 
-          DB.exec(<<~SQL, tl)
-            UPDATE users
-            SET title = groups.name
-            FROM groups
-            WHERE users.primary_group_id = groups.id
-              AND users.trust_level = ?
-              AND users.primary_group_id IS NOT NULL;
-          SQL
+        trust_levels.each do |tl|
+          if tl == 0
+            DB.exec(<<~SQL, tl, tl0_title)
+              UPDATE users
+              SET title = REPLACE(?, '{group_name}', groups.name)
+              FROM groups
+              WHERE users.primary_group_id = groups.id
+                AND users.trust_level = ?
+                AND users.primary_group_id IS NOT NULL;
+            SQL
+          elsif tl == 1
+            DB.exec(<<~SQL, tl, tl1_title)
+              UPDATE users
+              SET title = REPLACE(?, '{group_name}', groups.name)
+              FROM groups
+              WHERE users.primary_group_id = groups.id
+                AND users.trust_level = ?
+                AND users.primary_group_id IS NOT NULL;
+            SQL
+          elsif tl == 2
+            DB.exec(<<~SQL, tl, tl2_title)
+              UPDATE users
+              SET title = REPLACE(?, '{group_name}', groups.name)
+              FROM groups
+              WHERE users.primary_group_id = groups.id
+                AND users.trust_level = ?
+                AND users.primary_group_id IS NOT NULL;
+            SQL
+          elsif tl == 3
+            DB.exec(<<~SQL, tl, tl3_title)
+              UPDATE users
+              SET title = REPLACE(?, '{group_name}', groups.name)
+              FROM groups
+              WHERE users.primary_group_id = groups.id
+                AND users.trust_level = ?
+                AND users.primary_group_id IS NOT NULL;
+            SQL
+          elsif tl == 4
+            DB.exec(<<~SQL, tl, tl4_title)
+              UPDATE users
+              SET title = REPLACE(?, '{group_name}', groups.name)
+              FROM groups
+              WHERE users.primary_group_id = groups.id
+                AND users.trust_level = ?
+                AND users.primary_group_id IS NOT NULL;
+            SQL
+          end
+
+          # Append the TL text
+          # User.where(trust_level: 0).update_all("title = title || ' #{tl0_title}'") if tl0_title != ""
+          # User.where(trust_level: 1).update_all("title = title || ' #{tl1_title}'") if tl1_title != ""
+          # User.where(trust_level: 2).update_all("title = title || ' #{tl2_title}'") if tl2_title != ""
+          # User.where(trust_level: 3).update_all("title = title || ' #{tl3_title}'") if tl3_title != ""
+          # User.where(trust_level: 4).update_all("title = title || ' #{tl4_title}'") if tl4_title != ""
+        else
+          User.where(trust_level: 0).update_all(title: tl0_title) if tl0_title != ""
+          User.where(trust_level: 1).update_all(title: tl1_title) if tl1_title != ""
+          User.where(trust_level: 2).update_all(title: tl2_title) if tl2_title != ""
+          User.where(trust_level: 3).update_all(title: tl3_title) if tl3_title != ""
+          User.where(trust_level: 4).update_all(title: tl4_title) if tl4_title != ""
         end
-        # Append the TL text
-        User.where(trust_level: 0).update_all("title = title || ' #{tl0_title}'") if tl0_title != ""
-        User.where(trust_level: 1).update_all("title = title || ' #{tl1_title}'") if tl1_title != ""
-        User.where(trust_level: 2).update_all("title = title || ' #{tl2_title}'") if tl2_title != ""
-        User.where(trust_level: 3).update_all("title = title || ' #{tl3_title}'") if tl3_title != ""
-        User.where(trust_level: 4).update_all("title = title || ' #{tl4_title}'") if tl4_title != ""
       end
     end
   end
